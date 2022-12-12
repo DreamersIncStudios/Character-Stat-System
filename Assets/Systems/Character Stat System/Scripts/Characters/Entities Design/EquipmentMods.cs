f8f3705f334d6e77633cd00336675db56d6fd612
The Dreamer <boles4cg@hotmail.com>
2022-12-11 20:52:34 -0500

    correction made

--- Assets/Systems/Character Stat System/Scripts/Characters/Entities Design/EquipmentMods.cs
+++ Assets/Systems/Character Stat System/Scripts/Characters/Entities Design/EquipmentMods.cs
@@ -2,12 +2,13 @@ using Stats;
 using Stats.Entities;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
 namespace Stats.Entities
 {
-    public class EquipmentMods : MonoBehaviour
+    public partial class BaseCharacterComponent : IComponentData
     {
-        public void ModCharacterStats(BaseCharacterComponent character, List<StatModifier> Modifiers, bool Add)
+        public void ModCharacterStats( List<StatModifier> Modifiers, bool Add)
         {
             int MP = 1;
             if (!Add)
@@ -23,38 +24,38 @@ namespace Stats.Entities
                         Debug.LogWarning("Level Modding is not allowed at this time. Please contact Programming is needed");
                         break;
                     case AttributeName.Strength:
-                        character.GetPrimaryAttribute((int)AttributeName.Strength).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Strength).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Vitality:
-                        character.GetPrimaryAttribute((int)AttributeName.Vitality).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Vitality).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Awareness:
-                        character.GetPrimaryAttribute((int)AttributeName.Awareness).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Awareness).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Speed:
-                        character.GetPrimaryAttribute((int)AttributeName.Speed).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Speed).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Skill:
-                        character.GetPrimaryAttribute((int)AttributeName.Skill).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Skill).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Resistance:
-                        character.GetPrimaryAttribute((int)AttributeName.Resistance).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Resistance).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Concentration:
-                        character.GetPrimaryAttribute((int)AttributeName.Concentration).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Concentration).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.WillPower:
-                        character.GetPrimaryAttribute((int)AttributeName.WillPower).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.WillPower).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Charisma:
-                        character.GetPrimaryAttribute((int)AttributeName.Charisma).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Charisma).BuffValue += mod.BuffValue * MP;
                         break;
                     case AttributeName.Luck:
-                        character.GetPrimaryAttribute((int)AttributeName.Luck).BuffValue += mod.BuffValue * MP;
+                        GetPrimaryAttribute((int)AttributeName.Luck).BuffValue += mod.BuffValue * MP;
                         break;
                 }
             }
-            character.StatUpdate();
+            StatUpdate();
 
         }
     }
